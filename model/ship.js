bf3a11057a7f2a42becbae1db98079cef5463021
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2017-01-31 01:08:30 +0200

    Fixed formatting

--- model/ship.js
+++ model/ship.js
@@ -3,15 +3,15 @@ var mongoosastic = require('mongoosastic');
 var Schema = mongoose.Schema;
 
 var schema = new Schema({
-  buildnumber: {type: Number },
+  buildnumber: { type: Number },
   name: { type: String },
-  type_translated: {type: String},
-  type: {type: String },
-  type_info: {type: String},
-  buildyear: { type: String},
+  type_translated: { type: String },
+  type: { type: String },
+  type_info: { type: String },
+  buildyear: { type: String },
   properties: [{
-    property: {type: String},
-    value: {type: String}
+    property: { type: String },
+    value: { type: String }
   }]
 });
 
